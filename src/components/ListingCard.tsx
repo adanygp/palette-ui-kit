6011d5e0d973503612e8981639bc8530e7afe0d4
adanygp <adanyairgp@gmail.com>
2025-11-27 23:29:22 -0600

    listingCard Responsive

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -40,19 +40,25 @@ export const ListingCard = ({ colors,ListingCardData }: ListingCardProps) => {
             <div className="absolute bottom-4 left-4">
               <p className="rounded-3xl px-3 py-1.5 bg-[var(--color1)] text-[var(--color0)] font-medium text-sm">{ListingCardData.dealImageText}</p>
             </div>
+            <button className="group w-10 h-10 border border-[var(--color0)] rounded-full absolute top-4 right-2 cursor-pointer sm:hidden flex items-center justify-center">
+              <Icon name="heart" className="size-5 text-[var(--color0)] group-hover:size-6 transition-all duration-100 ease-linear transform origin-center" />
+            </button>
+            <button className="group w-10 h-10 border border-[var(--color0)] rounded-full absolute top-16 right-2 cursor-pointer sm:hidden flex items-center justify-center">
+              <Icon name="share" className="size-5 text-[var(--color0)] group-hover:size-6 transition-all duration-100 ease-linear transform origin-center" />
+            </button>
           </div>
           <div className="w-full lg:w-2/3 flex flex-col justify-between h-auto lg:h-[250px] rounded-r-3xl p-6 text-[var(--color3)]" >
-            <div className="flex flex-col sm:flex-row justify-between mb-3">
+            <div className="flex flex-row sm:mb-3 mb-4 justify-center sm:justify-between">
               <div>
-                <p className="text-2xl font-bold m-1">{ListingCardData.title}</p>
-                <div className="flex items-bottom gap-1">
+                <p className="text-2xl font-bold m-1 text-center">{ListingCardData.title}</p>
+                <div className="flex items-bottom gap-1 sm:justify-start justify-center">
                   <Icon name="star" className="size-6 text-[var(--color1)]" />
-                  <p className="text-xl font-bold">{ListingCardData.score}</p>
-                  <p className="text-[var(--color3)]/50 text-xl font-medium">({ListingCardData.scorevisits}) - </p>
-                  <p className="text-[var(--color3)]/80 text-xl font-medium">{ListingCardData.location}</p>
+                  <p className="sm:text-xl text-lg font-bold">{ListingCardData.score}</p>
+                  <p className="text-[var(--color3)]/50 sm:text-xl text-lg font-medium">({ListingCardData.scorevisits}) - </p>
+                  <p className="text-[var(--color3)]/80 sm:text-xl text-lg font-medium">{ListingCardData.location}</p>
                 </div>
               </div>
-              <div className="flex flex-col items-end">
+              <div className="flex flex-col items-end hidden sm:flex">
                 <div className="flex flex-row m-1 items-center">
                   <p className="rounded-3xl px-2 py-1 bg-[var(--color1)]/25 text-[var(--color0)] font-medium">Deal</p>
                   <p className="text-3xl font-bold ml-2">${ListingCardData.price}</p>
@@ -60,29 +66,35 @@ export const ListingCard = ({ colors,ListingCardData }: ListingCardProps) => {
                 <p className="m-1 text-[var(--color3)]/80 font-medium">{ListingCardData.priceComparison}</p>
               </div>
             </div>
-            <div className="flex flex-row mb-3 gap-4">
+            <div className="flex flex-row sm:mb-3 mb-4 gap-4 sm:justify-start justify-center">
               {ListingCardData.amenities.map((amenity, index) => (
-                <div key={index} className="flex flex-row items-center">
+                <div key={index} className="flex sm:flex-row flex-col items-center">
                   <Icon 
                     name={amenity.icon} 
                     className="size-6 text-[var(--color3)]/80 mr-1"
                   />
-                  <p className="text-[var(--color3)]/80 text-xl font-medium">{amenity.title}</p>
+                  <p className="text-[var(--color3)]/80 sm:text-xl text-md font-medium text-center">{amenity.title}</p>
                 </div>
               ))}
             </div>
-            <div className="flex flex-row gap-2 items-center justify-end">
-              <button className="group w-12 h-12 border border-[var(--color3)] rounded-full flex items-center justify-center cursor-pointer">
-                <Icon name="heart" className="size-6 m-auto stroke-[var(--color3)]/80 group-hover:size-7 group-hover:text-[var(--color3)]/80 transition-all duration-100 ease-linear" />
+            <div className="flex flex-row gap-2 items-center justify-center sm:justify-end">
+              <button className="group w-12 h-12 border border-[var(--color3)] rounded-full flex items-center justify-center cursor-pointer hidden sm:flex">
+                <Icon name="heart" className="size-6 stroke-[var(--color3)]/80 group-hover:size-7 transition-all duration-100 ease-linear transform origin-center" />
               </button>
-              <button className="group w-12 h-12 border border-[var(--color3)] rounded-full flex items-center justify-center cursor-pointer">
-                <Icon name="share" className="size-6 m-auto stroke-[var(--color3)]/80 group-hover:size-7 group-hover:text-[var(--color3)]/80 transition-all duration-100 ease-linear" />
+              <button className="group w-12 h-12 border border-[var(--color3)] rounded-full flex items-center justify-center cursor-pointer hidden sm:flex">
+                <Icon name="share" className="size-6 stroke-[var(--color3)]/80 group-hover:size-7 transition-all duration-100 ease-linear transform origin-center" />
               </button>
+              <div className="flex flex-col items-end sm:hidden">
+                <div className="flex flex-row m-1 items-center">
+                  <p className="text-4xl font-bold ml-2">${ListingCardData.price}</p>
+                </div>
+              </div>
               <button className="flex items-center gap-2 h-12 px-4 py-2 rounded-2xl text-white bg-[var(--color0)] hover:bg-[var(--color1)] transition-all duration-300 ease-linear cursor-pointer">
                 <p>Book Now</p>
                 <Icon name="calendar" className="size-7 m-auto stroke-white" />
               </button>
             </div>
+            <p className="m-1 text-[var(--color3)]/80 font-medium text-center sm:hidden text-sm">{ListingCardData.priceComparison}</p>
           </div>
         </div>
       </div>
